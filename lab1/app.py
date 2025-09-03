21b2619c94136c3f97cf96d8df6caf0535050b63
rodion <rodion.gorshkov.05@mail.ru>
2025-09-03 17:43:15 +0700

    Информация об авторе

--- lab1/app.py
+++ lab1/app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
-
+from flask import Flask
+from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
 
+environment = Environment(loader=FileSystemLoader("templates/"))
 
-@app.route("/")
-def start():
-    return render_template("base.html")
+@app.route("/web")
+def web():
+    template = environment.get_template('base.html')
+    return template.render()
 
+
+@app.route('/author') 
+def get_author():
+    template = environment.get_template("author.html")
+    data={
+        "name": "Родион",
+        "group": "ФБИ 31",
+        "faculty": "ФБ",
+    }                  
+    return template.render(data)
 
 
