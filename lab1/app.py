86d422b0a9d1937a2756cf50a6da991054fd4ebc
rodion <rodion.gorshkov.05@mail.ru>
2025-09-03 18:28:00 +0700

    Служебная информация

--- lab1/app.py
+++ lab1/app.py
@@ -1,49 +1,59 @@
-from flask import Flask, url_for
+import datetime
+from flask import Flask, redirect, request, url_for
 from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
 
 environment = Environment(loader=FileSystemLoader("templates/"))
 
+
 @app.route("/web")
 def web():
-    template = environment.get_template('base.html')
+    template = environment.get_template("base.html")
     return template.render()
 
 
-@app.route('/author') 
+@app.route("/author")
 def get_author():
     template = environment.get_template("author.html")
-    data={
+    data = {
         "name": "Родион",
         "group": "ФБИ 31",
         "faculty": "ФБ",
-    }                  
-    return template.render(data)
-
-@app.route('/image')
-def image():
-    template = environment.get_template("image.html")
-    image_path = url_for("static", filename="ЯнТоплес.jpeg")
-    data = {
-        "image_path": image_path
     }
     return template.render(data)
 
 
-
-count = 0
-@app.route('/counter')
-def counter():
-    template = environment.get_template('counter.html')
-    global count
-    count += 1 
-    data = {
-        "count": count
-    } 
+@app.route("/image")
+def image():
+    template = environment.get_template("image.html")
+    image_path = url_for("static", filename="ЯнТоплес.jpeg")
+    data = {"image_path": image_path}
     return template.render(data)
 
 
+count = 0
 
-    
-    
+@app.route("/counter")
+def counter():
+    template = environment.get_template("counter.html")
+    global count
+    count += 1
+    url = request.url
+    client_ip = request.remote_addr
+    time = datetime.datetime.today()
+    data = {
+        "count": count, 
+        "time": str(time), 
+        "client_ip": client_ip, 
+        "url": url
+    }
+    return template.render(data)
+
+@app.route("/reset")
+def clear_counter():
+    global count
+    count = 0
+    return redirect(url_for('counter'))
+
+
